e796c3b6f657a8a1f8341cc598b5804ea52cc2af
ldex <laurent.duveau+git@gmail.com>
2020-12-16 15:02:59 -0500

    Pagination

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map, startWith } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -14,8 +14,28 @@ export class ProductListComponent implements OnInit {
   title: string = 'Products';
 //  products: Product[];
   products$: Observable<Product[]>;
+  productsNb$: Observable<number>;
   selectedProduct: Product;
   errorMessage: string;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
@@ -36,7 +56,14 @@ export class ProductListComponent implements OnInit {
                             }
                           )
                         );
-
+    
+    this.productsNb$ = this
+                        .products$
+                        .pipe(
+                          map(products => products.length),
+                          startWith(0)
+                        );
+              
     // this
     //   .productService
     //   .products$
