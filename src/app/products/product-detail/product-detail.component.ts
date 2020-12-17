f67e72da41b85a6440a7962c60f511e6f31179fb
ldex <laurent.duveau+git@gmail.com>
2020-12-17 10:34:39 -0500

    End of the labs

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
 @Component({
@@ -10,9 +13,40 @@ export class ProductDetailComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) { }
+
+  delete() {
+    if(window.confirm('Are you sure ??')) {
+      this
+      .productService
+      .deleteProduct(this.product.id)
+      .subscribe(
+        () => {
+          console.log('Product was deleted!');
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        },
+        error => console.error('Could not delete product!')
+      )
+    }
+  }
 
   ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.params['id'];
+    this
+      .productService
+      .products$
+      .pipe(
+        map(products => products.find(product => product.id == id))
+      )
+      .subscribe(
+        result => this.product = result
+      )
+
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,12 +14,22 @@ export class ProductService {
 
   constructor(private http: HttpClient) {
     this.initProducts();
-   }
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(this.baseUrl + id);           
+  }
+
+  insertProduct(newProduct: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, newProduct);
+  }
 
   initProducts() {
+    let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
+
     this.products$ = this
                       .http
-                      .get<Product[]>(this.baseUrl)
+                      .get<Product[]>(url)
                       .pipe(
                         delay(1500),
                         tap(console.table),
